b6ae9da7fc466ad3b62e88fa6359ed4f47605422
youpaw <drozdov.vov@gmail.com>
2020-03-11 09:49:43 +0300

    cmake updated libft.h added

---
+++ libft.h
@@ -0,0 +1,20 @@
+//
+// Created by youpaw on 11.03.2020.
+//
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include "char_lib.h"
+#include "env_lib.h"
+#include "file_lib.h"
+#include "list_lib.h"
+#include "math_lib.h"
+#include "mem_lib.h"
+#include "num_lib.h"
+#include "regex_lib.h"
+#include "search_lib.h"
+#include "str_lib.h"
+#include "vec_lib.h"
+
+#endif //LIBFT_H
