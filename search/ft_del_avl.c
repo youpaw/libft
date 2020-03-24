ae0ef4dfb8ef41deb5e01f650a2d14f767e323d6
youpaw <drozdov.vov@gmail.com>
2020-03-24 12:00:55 +0300

    search_lib t_avl del fix

--- search/ft_del_avl.c
+++ search/ft_del_avl.c
@@ -4,14 +4,15 @@
 
 #include <stdlib.h>
 #include "search_lib.h"
+#include "mem_lib.h"
 
-void	ft_del_avl(t_avl **avl, void (*del)(void *))
+void	ft_del_avl(t_avl *avl, size_t size, void (*del)(void *, size_t))
 {
-	if (!avl || !*avl)
+	if (!avl)
 		return ;
-	ft_del_avl(&((*avl)->left), del);
-	ft_del_avl(&((*avl)->right), del);
-	del((*avl)->content);
-	free(*avl);
-	*avl = NULL;
+	ft_del_avl(avl->left, size, del);
+	ft_del_avl(avl->right, size, del);
+	del(avl->content, size);
+	free(avl);
+	ft_bzero(avl, sizeof(t_avl));
 }
