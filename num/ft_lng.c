915f7728beb1a38a0b3975b144a10d842f5d9b8c
youpaw <drozdov.vov@gmail.com>
2020-03-30 05:08:12 +0300

    ft_lng and ft_lng_base added, num module upd

---
+++ num/ft_lng.c
@@ -0,0 +1,10 @@
+//
+// Created by youpaw on 30.03.2020.
+//
+
+#include "num_lib.h"
+
+int ft_lng(ssize_t num)
+{
+	return (ft_lng_base(num, 10));
+}
--- num/num_lib.h
+++ num/num_lib.h
@@ -5,6 +5,10 @@
 #ifndef NUM_LIB_H
 # define NUM_LIB_H
 
+#include <zconf.h>
+
+int					ft_lng(ssize_t num);
+int					ft_lng_base(ssize_t num, int base);
 int					ft_atoi(const char *str);
 void				ft_putnbr(int n);
 void				ft_putnbr_fd(int n, int fd);
