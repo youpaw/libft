7de9a6fc3ca58ab609afadf9060a1c102936736b
Slava-Nya <svyatoslavank@gmail.com>
2020-10-31 15:45:11 +0300

    alias er print refactor

--- optparse/optparse.c
+++ optparse/optparse.c
@@ -12,7 +12,7 @@ static	size_t is_opt_invalid(const char *opt, const char *validopt)
 			return ((size_t)(*opt));
 		opt++;
 	}
-	return ((size_t) '0');
+	return (0);
 }
 
 static size_t is_option(const char *opt)
