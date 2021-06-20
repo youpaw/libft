e929bb3c78fd4bfa2d889c975492368d518d3316
dbutterw <dbutterw@student.21-school.ru>
2021-06-20 17:52:44 +0300

    Fix norme

--- memory/swap_int.c
+++ memory/swap_int.c
@@ -4,18 +4,18 @@
 
 #include <stdint.h>
 
-int16_t		ft_swap_int16(int16_t x)
+int16_t	ft_swap_int16(int16_t x)
 {
 	return ((x >> 8) | (x << 8));
 }
 
-int32_t		ft_swap_int32(int32_t x)
+int32_t	ft_swap_int32(int32_t x)
 {
 	x = ((x >> 8) & 0x00FF00FF) | ((x << 8) & 0xFF00FF00);
 	return ((x >> 16) | (x << 16));
 }
 
-int64_t		ft_swap_int64(int64_t x)
+int64_t	ft_swap_int64(int64_t x)
 {
 	x = ((x >> 8) & 0x00FF00FF00FF00FFULL) | ((x << 8) & 0xFF00FF00FF00FF00ULL);
 	x = ((x >> 16) & 0x0000FFFF0000FFFFULL) | \
